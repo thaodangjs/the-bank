8219db4ae4aba18f42c85dd407234c94c156cb1c
Thao Dang <thaodang.js@gmail.com>
2023-12-19 09:22:56 +0700

    Professional Redux File Structure State Slices - added little code in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import "./store-v1";
+import store from "./store-v1";
+import { deposit } from "./features/accounts/accountSlice";
+
+store.dispatch(deposit(200));
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
