26f9f11131d405755093e2f9074c8fa68ec8cb2c
Thao Dang <thaodang.js@gmail.com>
2023-12-20 11:16:37 +0700

    slice customer - finish

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,9 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const initialStateCustomer = {
   fullName: "",
   nationalID: "",
   createdAt: "",
 };
 
+const customerSlice = createSlice({
+  name: "account",
+  initialState: initialStateCustomer,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+export const { createCustomer, updateName } = customerSlice.actions;
+
+export default customerSlice.reducer;
+
+/*
 export default function customerReducer(state = initialStateCustomer, action) {
   switch (action.type) {
     case "customer/createCustomer":
@@ -37,3 +69,4 @@ export const createCustomer = (fullName, nationalID) => {
 export const updateName = (fullName) => {
   return { type: "customer/updateName", payload: fullName };
 };
+*/
