30453e4b68eca0ea4dba78813616901a1563f99b
Thao Dang <thaodang.js@gmail.com>
2023-12-20 11:03:09 +0700

    slice with RTK

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,9 +1,65 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
+
+const accountSlice = createSlice({
+  name: "account",
+  initialState: initialStateAccount,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+      state.isLoading = false;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      prepare(amount, purpose) {
+        return { payload: { amount, purpose } };
+      },
+      reducer(state, action) {
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance += action.payload.amount;
+      },
+    },
+    //Vì requestLoan có nhiều tham số hơn 1, ta cần dùng prepare() để chuẩn bị tham số
+    payLoan(state) {
+      state.balance -= state.loan;
+      state.loan = 0; // Dòng này trên cùng sẽ lỗi
+      state.loanPurpose = "";
+    },
+    //Vì code mutate, ta cần chú ý thứ tự dòng nào nên ở trên, dòng nào ở dưới
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
+  },
+});
+
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions; //export các creator function
+
+export const deposit = (amount, currency) => {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+  return async function (dispatch) {
+    dispatch({ type: "account/convertingCurrency" });
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    console.log(data);
+    const converted = data.rates.USD;
+    dispatch({ type: "account/deposit", payload: converted });
+  };
+};
+
+export default accountSlice.reducer; //export state và action, console accountSlice để rõ chi tiết
+
+/*
 
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
@@ -65,3 +121,4 @@ export const requestLoan = (num, purpose) => {
 export const payLoan = () => {
   return { type: "account/payLoan" };
 };
+*/
