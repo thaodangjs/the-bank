29c40bd91ad0bd726c4c31bb20021a2b99fd2bb2
Thao Dang <thaodang.js@gmail.com>
2023-12-20 16:47:43 +0700

    configureStore with RTK

--- src/store.js
+++ src/store.js
@@ -1,18 +1,12 @@
-import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customer/customerSlice";
-import { thunk } from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
 
 export default store;
